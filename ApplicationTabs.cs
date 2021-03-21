fbce60db6d9d9c21f70876e78159a5f2d906e656
Planeta Aragubas <dpaulootavio5@outlook.com>
2021-03-21 10:42:01 -0300

    Removed useless imports

---
+++ ApplicationTabs.cs
@@ -0,0 +1,133 @@
+﻿/*
+   Copyright 2021 Aragubas/Paulo Otávio de Lima
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. 
+  
+ */
+
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace TNotepad
+{
+    public partial class ApplicationTabs : TabControl
+    {
+        public ApplicationTabs()
+        {
+            //if (!DesignMode) Multiline = true;
+            this.DrawMode = TabDrawMode.OwnerDrawFixed;
+            this.Appearance = TabAppearance.Buttons;
+        }
+
+        private const int TCM_ADJUSTRECT = 0x1328;
+
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+
+            base.OnPaintBackground(pevent);
+            pevent.Graphics.FillRectangle(Brushes.Black, new Rectangle(0, 0, Width, Height));
+
+
+        }
+
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            //base.OnDrawItem(e);
+
+            Font fntTab;
+            Brush bshBack;
+            Brush bshFore;
+
+            // Set SmoothingMode
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+
+            if (e.Index == this.SelectedIndex) // Selected Tab
+            {
+                fntTab = new Font(e.Font, FontStyle.Underline);
+                bshFore = Brushes.White;
+                bshBack = new SolidBrush(Color.FromArgb(255, 64, 64, 64));
+
+            }
+            else // Unselected Tab
+            {
+                fntTab = e.Font;
+                bshBack = new SolidBrush(Color.FromArgb(255, 32, 32, 32));
+                bshFore = new SolidBrush(Color.White);
+
+            }
+
+            // Draw Title Background
+            e.Graphics.FillRectangle(bshBack, e.Bounds);
+
+            // Add spacing to title text
+            if (!TabPages[e.Index].Text.EndsWith("   "))
+            {
+                TabPages[e.Index].Text += "   ";
+
+            }
+
+            // Draw Tab Title
+            string tabName = TabPages[e.Index].Text;
+            StringFormat sftTab = new StringFormat();
+            Rectangle recTab = e.Bounds;
+            recTab = new Rectangle(recTab.X, recTab.Y + 4, recTab.Width, recTab.Height - 4);
+
+            e.Graphics.DrawString(tabName, fntTab, bshFore, recTab, sftTab);
+
+            // Draw Red Circle
+            var imageRect = new Rectangle(recTab.Right - 13, recTab.Top + (recTab.Height - 12) / 2, 10, 10);
+            e.Graphics.FillEllipse(Brushes.Red, imageRect);
+
+            // Draw Background
+            Rectangle r = GetTabRect(this.TabPages.Count - 1);
+
+            RectangleF tf = new RectangleF(r.X + r.Width, r.Y - 5, this.Width - (r.X + r.Width) + 5, r.Height + 5);
+            Brush b = new SolidBrush(Color.FromArgb(255, 32, 32, 32));
+
+            e.Graphics.FillRectangle(b, tf);
+
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // Hide all borders
+            if (m.Msg == TCM_ADJUSTRECT)
+            {
+                RECT rect = (RECT)(m.GetLParam(typeof(RECT)));
+                rect.Left = this.Left - 4;
+                rect.Right = this.Right + 4;
+
+                rect.Top = this.Top - 6;
+                rect.Bottom = this.Bottom + 4;
+
+                Marshal.StructureToPtr(rect, m.LParam, true);
+                //m.Result = (IntPtr)1;
+                //return;
+            }
+
+
+            //else
+            // call the base class implementation
+            base.WndProc(ref m);
+        }
+
+        private struct RECT
+        {
+            public int Left, Top, Right, Bottom;
+        }
+    }
+
+}
--- Form1.cs
+++ Form1.cs
@@ -16,15 +16,7 @@
  */
 
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TNotepad
@@ -130,6 +122,7 @@ namespace TNotepad
             AttachSidePanel();
 
             Text = "TNotepad " + Utils.GetVersion();
+
         }
 
         public void CloseSelectedTab()
@@ -163,35 +156,14 @@ namespace TNotepad
 
         private void Tabs_DrawItem(object sender, DrawItemEventArgs e)
         {
-            /*
-            SolidBrush fillBrush = new SolidBrush(Color.DimGray);
-
-
-
-            //draw rectangle behind the tabs
-            Rectangle lasttabrect = Tabs.GetTabRect(Tabs.TabPages.Count - 1);
-            Rectangle background = new Rectangle();
-            background.Location = new Point(lasttabrect.Right, 0);
-
-            //pad the rectangle to cover the 1 pixel line between the top of the tabpage and the start of the tabs
-            background.Size = new Size(Tabs.Right - background.Left, lasttabrect.Height + 3);
-            e.Graphics.FillRectangle(fillBrush, background);
-            */
-
-            var tabPage = Tabs.TabPages[e.Index];
+            //var tabPage = Tabs.TabPages[e.Index];
             var tabRect = Tabs.GetTabRect(e.Index);
             tabRect.Inflate(-2, -2);
-            
-            // Draw tab title
-            //e.Graphics.DrawString(tabPage.Text, SystemFonts.DefaultFont, Brushes.Red, new Point(tabRect.X, tabRect.Y));
 
             var closeImage = new Bitmap(Properties.Resources.CloseTabMini);
             e.Graphics.DrawImage(closeImage,
                 (tabRect.Right - 8),
                 tabRect.Top + (tabRect.Height - 8) / 2, 8, 8);
-
-            //TextRenderer.DrawText(e.Graphics, tabPage.Text, tabPage.Font,
-            //    tabRect, Color.DarkBlue, TextFormatFlags.Left);
 
         }
 
@@ -219,111 +191,5 @@ namespace TNotepad
         }
     }
 
-    public partial class ApplicationTabs : TabControl
-    {
-        public ApplicationTabs()
-        {
-            //if (!DesignMode) Multiline = true;
-            this.DrawMode = TabDrawMode.OwnerDrawFixed;
-            this.Appearance = TabAppearance.Buttons;
-        }
-
-        private const int TCM_ADJUSTRECT = 0x1328;
-
-        protected override void OnPaintBackground(PaintEventArgs pevent)
-        {
-
-            base.OnPaintBackground(pevent);
-            pevent.Graphics.FillRectangle(Brushes.Black, new Rectangle(0, 0, Width, Height));
-
-
-        }
-
-        
-        protected override void OnDrawItem(DrawItemEventArgs e)
-        {
-            //base.OnDrawItem(e);
-    
-            Font fntTab;
-            Brush bshBack;
-            Brush bshFore;
-
-            // Set SmoothingMode
-            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
-
-            if (e.Index == this.SelectedIndex) // Selected Tab
-            {
-                fntTab = new Font(e.Font, FontStyle.Underline);
-                bshFore = Brushes.White;
-                bshBack = new SolidBrush(Color.FromArgb(255, 64, 64, 64));
-
-            }
-            else // Unselected Tab
-            {
-                fntTab = e.Font;
-                bshBack = new SolidBrush(Color.FromArgb(255, 32, 32, 32));
-                bshFore = new SolidBrush(Color.White);
-
-            }
-
-            // Draw Title Background
-            e.Graphics.FillRectangle(bshBack, e.Bounds);
-
-            // Add spacing to title text
-            if (!TabPages[e.Index].Text.EndsWith("   "))
-            {
-                TabPages[e.Index].Text += "   ";
-
-            }
-
-            // Draw Tab Title
-            string tabName = TabPages[e.Index].Text;
-            StringFormat sftTab = new StringFormat();
-            Rectangle recTab = e.Bounds;
-            recTab = new Rectangle(recTab.X, recTab.Y + 4, recTab.Width, recTab.Height - 4);
-            
-            e.Graphics.DrawString(tabName, fntTab, bshFore, recTab, sftTab);
-
-            // Draw Red Circle
-            var imageRect = new Rectangle(recTab.Right - 13, recTab.Top + (recTab.Height - 12) / 2, 10, 10);
-            e.Graphics.FillEllipse(Brushes.Red, imageRect);
-
-            // Draw Background
-            Rectangle r = this.GetTabRect(this.TabPages.Count - 1);
-
-            RectangleF tf = new RectangleF(r.X + r.Width, r.Y - 5, this.Width - (r.X + r.Width) + 5, r.Height + 5);
-            Brush b = new SolidBrush(Color.FromArgb(255, 32, 32, 32));
-
-            e.Graphics.FillRectangle(b, tf);
-
-        }
-
-        protected override void WndProc(ref Message m)
-        {
-            // Hide all borders
-            if (m.Msg == TCM_ADJUSTRECT)
-            {
-                RECT rect = (RECT)(m.GetLParam(typeof(RECT)));
-                rect.Left = this.Left - 4;
-                rect.Right = this.Right + 4;
-
-                rect.Top = this.Top - 6;
-                rect.Bottom = this.Bottom + 4;
-
-                Marshal.StructureToPtr(rect, m.LParam, true);
-                //m.Result = (IntPtr)1;
-                //return;
-            }
-            //else
-            // call the base class implementation
-            base.WndProc(ref m);
-        }
-
-        private struct RECT
-        {
-            public int Left, Top, Right, Bottom;
-        }
-    }
 
 }
